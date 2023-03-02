7776c3de57e00eba8a33b7b3b0c5204af096b9df
ziemba92PL <ziemba92Pl@GMIAL.COM>
2023-03-02 20:56:01 +0100

    add function

--- money.js
+++ money.js
@@ -1,36 +1,25 @@
+let currency = [
+  4.71,
+  4.45,
+  5.35,
+  4.72,
+];
+
 let EURValue = 4.71;
 let USDValue = 4.45;
 let GBPValue = 5.35;
 let CHFValue = 4.72;
 
+function printValue(plnValue, currencyIndex, c) {
+  console.log(plnValue + " zł to Wartość w Euro to: " +
+    c[currencyIndex] + " EUR");
+}
+
 console.log("Przelicz złotówki na inne waluty");
 console.log("1. Euro  2. Dolar  3. Funt  4. Frank Szwajcarski");
-let exchange = prompt("Wybierz walutę");
+let exchange = parseInt(prompt("Wybierz walutę"));
 
-if (exchange == 1) {
-  console.log(" Podaj kwotę w złotówkach: ");
-  let PLNValue = parseInt(prompt("Podaj kwotę w złotówkach: "));
+console.log(" Podaj kwotę w złotówkach: ");
+let PLNValue = parseInt(prompt("Podaj kwotę w złotówkach: "));
 
-  console.log(" Wartość w Euro to: " + PLNValue / EURValue + " EUR");
-}
-
-if (exchange == 2) {
-  console.log(" Podaj kwotę w złotówkach: ");
-  let PLNValue = parseInt(prompt("Podaj kwotę w złotówkach: "));
-
-  console.log(" Wartość w USD to: " + PLNValue / USDValue + " USD");
-}
-
-if (exchange == 3) {
-  console.log(" Podaj kwotę w złotówkach: ");
-  let PLNValue = parseInt(prompt("Podaj kwotę w złotówkach: "));
-
-  console.log(" Wartość w GBP to: " + PLNValue / GBPValue + " GBP");
-}
-
-if (exchange == 4) {
-  console.log(" Podaj kwotę w złotówkach: ");
-  let PLNValue = parseInt(prompt("Podaj kwotę w złotówkach: "));
-
-  console.log(" Wartość w CHF to: " + PLNValue / CHFValue + " CHF");
-}
+printValue(PLNValue, exchange - 1, currency);
